6333d86418979f62cf030a03f42bfa62c9aa0aa3
Nikolai Telin <telinnikolai@gmail.com>
2024-01-21 02:41:20 +0000

    added local server with the ability to upload .xlsx file and select sheet

---
+++ excelConverter.py
@@ -0,0 +1,87 @@
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+
+
+#df = pd.read_excel("Sankey ARR.xlsx", sheet_name="Labels", skiprows=[])
+
+def create_sankey_diagram(df):
+    #xls = pd.ExcelFile("Sankey ARR.xlsx")
+
+
+    #converting columns from Excel to arrays
+    label = df.iloc[:, 0].to_numpy()
+    target = df.iloc[:, 1].to_numpy()
+
+    combined_data = np.concatenate((label, target))
+
+    value = df.iloc[:, 2].to_numpy()
+    filtered_array = value[~np.isnan(value)]
+
+
+    #dictionary of unique strings
+    unique_dict = {}
+
+    label_names = []
+
+    item_number = 0
+
+    for item in combined_data:
+        if not pd.isna(item) and item not in unique_dict:
+            unique_dict[item] = item_number
+            label_names.append(item)
+            item_number += 1
+
+
+    #print(label_names)
+
+    #Replacing elements in label and target with numbers from the dictionary
+    label_numbers = [unique_dict[item] for item in label if not pd.isna(item)]
+    target_numbers = [unique_dict[item] for item in target if not pd.isna(item)]
+
+    '''
+    for item, number in unique_dict.items():
+        print(f"{item}: №{number}")
+
+    print(len(unique_dict))
+
+
+    print(label_numbers)
+
+    print(target_numbers)
+
+
+    for item in value:
+        if not pd.isna(item):
+            print(item)
+    '''
+
+
+    fig = go.Figure(go.Sankey(
+
+        #"snap" means that , nodes are automatically adjusted
+        #to reduce the intersection of links
+        #and improve the readability of the diagram
+
+        arrangement = "snap",
+
+        node = {
+            "label": label_names,
+            "pad": 15,
+            "thickness": 20,
+            "line": {"color": "black", "width": 0.5}
+        },
+
+        link = {
+            "source": label_numbers,
+            "target": target_numbers,
+            "value": filtered_array
+        }
+    ))
+
+    fig.update_layout(title_text="Sankey for Andrei", font_size=10)
+    fig.show()
+
+    return fig
+
+#create_sankey_diagram(df)
--- main.py
+++ main.py
@@ -1,81 +1,30 @@
-import plotly.graph_objects as go
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import HTMLResponse
+from io import BytesIO
 import pandas as pd
-import numpy as np
+from excelConverter import create_sankey_diagram
 
-xls = pd.ExcelFile("Sankey ARR.xlsx")
+app = FastAPI()
 
-df = pd.read_excel("Sankey ARR.xlsx", sheet_name="Labels", skiprows=[])
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...), sheet_name: str = 'Sheet 1'):
+    try:
+        #read file into memory
+        contents = await file.read()
 
-#converting columns from Excel to arrays
-label = df.iloc[:, 0].to_numpy()
-target = df.iloc[:, 1].to_numpy()
+        # using BytesIO
+        df = pd.read_excel(BytesIO(contents), sheet_name=sheet_name)
 
-combined_data = np.concatenate((label, target))
+        # call create_sankey_diagram(
+        fig = create_sankey_diagram(df)
 
-value = df.iloc[:, 2].to_numpy()
-filtered_array = value[~np.isnan(value)]
+        # translate to html
+        fig.write_html("sankey_diagram.html")
 
+        return HTMLResponse(content="<html><body><h1>Sankey chart was created</h1></body></html>", status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-#dictionary of unique strings
-unique_dict = {}
-
-label_names = []
-
-item_number = 0
-
-for item in combined_data:
-    if not pd.isna(item) and item not in unique_dict:
-        unique_dict[item] = item_number
-        label_names.append(item)
-        item_number += 1
-
-
-#print(label_names)
-
-#Replacing elements in label and target with numbers from the dictionary
-label_numbers = [unique_dict[item] for item in label if not pd.isna(item)]
-target_numbers = [unique_dict[item] for item in target if not pd.isna(item)]
-
-'''
-for item, number in unique_dict.items():
-    print(f"{item}: №{number}")
-
-print(f"\nКоличество уникальных элементов в объединенных столбцах (исключая null): {len(unique_dict)}")
-
-print("\nПреобразованные данные из label:")
-print(label_numbers)
-
-print("\nПреобразованные данные target:")
-print(target_numbers)
-
-
-for item in value:
-    if not pd.isna(item):
-        print(item)
-'''
-
-
-fig = go.Figure(go.Sankey(
-
-    #"snap" means that , nodes are automatically adjusted
-    #to reduce the intersection of links
-    #and improve the readability of the diagram
-
-    arrangement = "snap",
-
-    node = {
-        "label": label_names,
-        "pad": 15,
-        "thickness": 20,
-        "line": {"color": "black", "width": 0.5}
-    },
-
-    link = {
-        "source": label_numbers,
-        "target": target_numbers,
-        "value": filtered_array
-    }
-))
-
-fig.update_layout(title_text="Sankey for Andrei", font_size=10)
-fig.show()
+@app.get("/")
+def read_root():
+    return {"message": "Upload your xlsx file here"}
